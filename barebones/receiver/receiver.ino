b277af9fe36ee5044d82594e09568404bf4ff787
PrestonXPitzer <prestonpitzer20@vt.edu>
2023-03-29 14:31:24 -0400

    updated these both with parity functionality

--- barebones/receiver/receiver.ino
+++ barebones/receiver/receiver.ino
@@ -18,16 +18,35 @@ void setup() {
   pinMode(PIN_NUM, INPUT);
   Serial.begin(9600);
 }
-
 void loop() {
   previous = current;
   current = pseudoDigiRead(PIN_NUM);
+  int bits[8];
   if (current == 0 && previous == 1) {
     delay(15);
     for (int i = 7; i >= 0; i--) {
-      test |= ((pseudoDigiRead(PIN_NUM)) << i);
+      char in = pseudoDigiRead(PIN_NUM);
+      bits[i] = in;
+      test |= (in << i);
       delay(11);
     }
+    //grab the parity bit seperately for easier calculation
+    int parityBit = pseudoDigiRead(PIN_NUM);
+    delay(11);
+    //determine the number of 1 bits that we got
+    int posBits = 0;
+    for (int j = 0;j<8;j++){
+      if (bits[j] == 1){
+        posBits++;
+      }
+    }
+    //if with the parity bit we have an even number than we're chillin
+    //otherwise notify the user that we are not chillin
+    if ((posBits + parityBit) % 2 != 0){ 
+      Serial.print("[likely error]");
+    }
+    
+    
     Serial.print(test);
     test = 0;
   }
