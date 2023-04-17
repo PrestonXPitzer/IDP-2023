96b417a626e128372a3f26a18d5e6fc7125bc834
PrestonXPitzer <prestonpitzer20@vt.edu>
2023-04-17 14:38:04 -0400

    added comments and   fixed what was printed lel

--- barebones/receiver/receiver.ino
+++ barebones/receiver/receiver.ino
@@ -4,6 +4,8 @@
 int read_val = -1;
 char test = 0;
 char previous = 1, current = 1;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 4;
 
 char pseudoDigiRead(int pin) {
   read_val = analogRead(PIN_NUM);
@@ -51,33 +53,50 @@ byte checkParity(byte in){
 /**
  * 
  */
-char hammingDecode(bool frame[11]){
-  bool R1 = (frame[0]^frame[2]^frame[4]^frame[6]^frame[8]^frame[10]);
-  bool R2 = frame[1]^frame[2]^frame[5]^frame[6]^frame[9]^frame[10]);
-  bool R4 = (frame[3]^frame[4]^frame[5]^frame[6]);
-  bool R8 = (frame[7]^frame[8]^frame[9]^frame[10]);
-  int error = (R1+(2*R2)+(4*R4)+(8*R8));
-  if (error != 0){
-    frame[error-1] = !frame[error-1];
+char hammingDecode(int frame[11]){
+  int R1 = (frame[0]^frame[2]^frame[4]^frame[6]^frame[8]^frame[10]); //check group 1
+  int R2 = (frame[1]^frame[2]^frame[5]^frame[6]^frame[9]^frame[10]); //check group 2
+  int R4 = (frame[3]^frame[4]^frame[5]^frame[6]); //check group 3
+  int R8 = (frame[7]^frame[8]^frame[9]^frame[10]); //check group 4
+  int error = (R1+(2*R2)+(4*R4)+(8*R8)); //the error location is the decimal representation
+                                         //of the binary value formed by the parity bits. 
+  if (error != 0){ //if the error exists
+    if (frame[error-1] == 1){
+      frame[error-1] = 0; //then correct it by flipping the bit
+    }
+    else {
+      frame[error-1] = 1;
+    }
   }
   char outputCharacter = (frame[2]+2*frame[4]+4*frame[5]+8*frame[6]+16*frame[8]+32*frame[9]+64*frame[10]);
-  print(outputCharacter);
+  Serial.print(outputCharacter);
 }
+
+/**
+ * setup the pins
+ */
 void setup() {
   pinMode(PIN_NUM, INPUT);
   Serial.begin(9600);
 }
+
+/**
+ * mainloop
+ */
 void loop() {
-  bool frame[11];
-  previous = current;
-  current = pseudoDigiRead(PIN_NUM);
-  if (current == 0 && previous == 1) {
-    delay(15);
-    for (int i = 10; i >= 0; i--) {
-      char in = pseudoDigiRead(PIN_NUM);
-      frame[i] = in;
-      delay(11);
+  int frame[11];
+  previous = current; //the last value from the loop should be our new previous
+  current = pseudoDigiRead(PIN_NUM); //read a new value for comparison
+  if (current == 0 && previous == 1) { //if we read a 1 to 0 transition
+    lastDebounceTime = millis(); //start timing from that point
+  if ((millis() - lastDebounceTime) > debounceDelay){//if the 0 doesn't go back to a 1
+        delay(15-debounceDelay); //then we know that the signal is real
+    for (int i = 10; i >= 0; i--) { //for each of the bits in the frame
+      char in = pseudoDigiRead(PIN_NUM); //read the current input state
+      frame[i] = in; //hold this data in the frame
+      delay(11); //wait for the next bit
     }
-    hammingDecode(test);
+  }
+    hammingDecode(frame);
   }
 }
--- barebones/transmitter/transmitter.ino
+++ barebones/transmitter/transmitter.ino
@@ -7,10 +7,10 @@ void setup() {
 int baud = 22;
 void character_wise_transmission(char character, int pin){
   //Convert the integer char value into it's ascii equivalent, using the same process that we use to convert dec to bin by hand
-  bool bits[7]; //array to hold the character 
-  bool frame[11]; //array to hold the hamming code modified 
-  for (int i = 6; i >= 0; i--) {
-    bits[i] = (character & (1 << i));
+  int frame[11]; //array to hold the hamming code modified 
+  int bits[7];
+  for (int i = 7; i >= 0; i--) {
+    bits[i] = (character & (1 << i)) != 0;
   }
   //fill in the hamming frame with the character bits
   frame[10] = bits[6];
@@ -20,17 +20,17 @@ void character_wise_transmission(char character, int pin){
   frame[5] = bits[2];
   frame[4] = bits[1];
   frame[2] = bits[0];
-  frame[0] = (frame[2]^frame[4]^frame[6]^frame[8]^frame[10]);
-  frame[1] = (frame[1]^frame[2]^frame[5]^frame[6]^frame[9]^frame[10]);
-  frame[3] = (frame[3]^frame[4]^frame[5]^frame[6]);
-  frame[7] = (frame[7]^frame[8]^frame[9]^frame[10]);
+  frame[0] = (frame[2]^frame[4]^frame[6]^frame[8]^frame[10]) != 0;
+  frame[1] = (frame[1]^frame[2]^frame[5]^frame[6]^frame[9]^frame[10]) != 0;
+  frame[3] = (frame[3]^frame[4]^frame[5]^frame[6]) != 0;
+  frame[7] = (frame[7]^frame[8]^frame[9]^frame[10]) != 0;
   //Send the first low bit, at 45.45 baud, the duty cycle per bit is going to be .022 or 22ms always. 
   //For all transmissions 2125Hz = 0, 2295Hz = 1
   digitalWrite(pin, 0);
   delay(11);
   for (int j = 10; j >= 0; j--) {
-    digitalWrite(pin, bits[j]);
-    Serial.print(bits[j]);
+    digitalWrite(pin, frame[j]);
+    Serial.print(frame[j]);
     delay(11);
   }
   Serial.println();
