db8d4b415480c02ebda6e4bff676d849c079c2b0
Dawson Schraiber <dschraiber@vt.edu>
2023-04-17 21:05:01 -0400

    Added initial debounce

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -12,6 +12,9 @@ int read_val = -1;
 char test = 0;
 char previous = 1, current = 1;
 
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 4;
+
 char pseudoDigiRead(int pin) {
   read_val = analogRead(pin);
   if (read_val >= THRESHOLD) {
@@ -67,7 +70,10 @@ void loop() {
   previous = current;
   current = pseudoDigiRead(RECV_PIN);
   if (current == 0 && previous == 1) {
-    delay(half_period * 3 / 2);
+    lastDebounceTime = millis(); //start timing from that point
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay && current == 0) { //if the 0 doesn't go back to a 1
+    delay((half_period * 3 / 2) - debounceDelay);  //then we know that the signal is real
     for (int i = (PACKET_SIZE - 1); i >= 0; i--) {
       packet |= (pseudoDigiRead(RECV_PIN) << i);
       delay(half_period);
